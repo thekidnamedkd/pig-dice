c417ce3d7f5349d2ed80c5c8e4b53a191f3da60b
Kevin Davis <thekidnamedkd@gmail.com>
2020-06-17 16:01:29 -0700

    establish working functionality for player 1 dice and turn score rules.
    Co-authored-by: Vanessa Guerrero <vamarilug@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,20 +5,30 @@ function Player() {
   this.player = ""
 }
 
-
 // business logic for player object
-Player.prototype.rollDice = function(dice) {
-  let dice = Math.floor(Math.random() * 7) + 1;
+Player.prototype.rollDice = function() {
+  let dice = Math.floor(Math.random() * 6) + 1;
+  if (dice === 1) {
+  this.turnScore = 0;
+  } else if (dice !== 1) {
   this.turnScore += dice
+  }
   console.log(dice)
 }
 
 // User Interface --
-$(document).ready(function() {
-  $("form#gameboard").submit(function(event) {
-    event.preventDefault();
-    let player1 = new Player()
-    let player2 = new Player()
+$(document).ready(function(event) {
+    let player1 = new Player();
+    let player2 = new Player();
 
+    $("button#roll1").click(function() {
+      player1.rollDice();
+      $(".p1-temp-score").html(player1.turnScore)
+      $(".p1-dice-output").html(Player.rollDice)
+    });
 
-}})
+    $("button#roll2").click(function() {
+      player2.rollDice();
+      $(".p2-temp-score").html(player2.turnScore)
+    }); 
+});
